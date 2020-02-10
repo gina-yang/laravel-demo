1e5af5878941a21732e922dec4063ffac2d22f56
Gina Yang <ginayang@usc.edu>
2020-02-10 15:26:24 -0800

    added sqlite database

---
+++ app/Http/Controllers/InvoiceController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class InvoiceController extends Controller
+{
+    public function index(){
+        // $invoices = DB::table('invoices')->get();
+        // dd($invoices);
+        return view('invoice.index');
+    }
+}
